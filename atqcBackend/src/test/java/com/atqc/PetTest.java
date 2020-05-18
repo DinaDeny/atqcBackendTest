c4e5dd708c08ba9aed34ad5285aea2d856bf1b99
DinaDeny <denisenkodina20@gmail.com>
2020-05-18 23:33:37 +0300

    add new tests add data provider

--- atqcBackend/src/test/java/com/atqc/PetTest.java
+++ atqcBackend/src/test/java/com/atqc/PetTest.java
@@ -2,6 +2,7 @@ package com.atqc;
 
 import io.qameta.allure.Description;
 import lombok.extern.log4j.Log4j;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,7 +11,7 @@ import static org.hamcrest.Matchers.*;
 public class PetTest extends RestAPIBaseTest {
 
     @Test
-    @Description("Create Pet with info")
+    @Description("Create Pet with Name and status")
 
     public void createNewValidPet() {
 
@@ -46,4 +47,128 @@ public class PetTest extends RestAPIBaseTest {
                        .body("status", is("available"))
                        .body("id", notNullValue());
                  }
+
+
+
+    @Test
+    @Description("Create Pet with Name and status")
+
+    public void createNewValidPet1() {
+
+        given()
+                .contentType("application/json")
+                .baseUri("https://petstore.swagger.io/v2")
+                .header("Access-Token", "1111222validtoken33344444")
+                .body("{\n" +
+                        "  \"id\": 2,\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 2,\n" +
+                        "    \"name\": \"dogs\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"Muchtar\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"available\"\n" +
+                        "}")
+
+                .when()
+                .post("/pet")
+
+                .then()
+                .statusCode(200)
+                .body("name", is("Muchtar"))
+                .body("status", is(not(emptyString())))
+                .body("id", is(2));
+    }
+
+    @Test
+    @Description("Create Pet with Name and status")
+
+    public void createNewValidPet3() {
+
+        String petName = "Murka";
+       String status = given()
+                .contentType("application/json")
+                .baseUri("https://petstore.swagger.io/v2")
+                .header("Access-Token", "1111222validtoken33344444")
+                .body("{\n" +
+                        "  \"id\": 3,\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 3,\n" +
+                        "    \"name\": \"cats\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"Murka\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"pending\"\n" +
+                        "}")
+
+                .when()
+                .post("/pet")
+
+                .then()
+                .statusCode(200)
+                .body("name", equalTo(petName))
+                .body("status", is("pending"))
+                .body("id", notNullValue())
+                .extract().path("status");
+       System.out.println("Pet status is: " + status);
+    }
+
+    @Test
+    @Description("Create Pet with invalid category")
+    public void createPetwithInvalidCategory(){
+
+        given()
+                .contentType("application/json")
+                .baseUri("https://petstore.swagger.io/v2")
+                .header("Access-Token", "1111222validtoken33344444")
+        .when()
+                .post("/pet/flying")
+        .then()
+                .statusCode(415); //когда ввожу "405" номер ошибки (как в примере в свагере), то фейлиться тест - ??
+    }
+
+     @Test(dataProvider = "Invalid statuses")
+     @Description("Get pet by invalid status")
+     public void getPetByInvalidStatus(String petStatus, int code, String message) {
+
+        given()
+                .contentType("application/json")
+                .baseUri("https://petstore.swagger.io/v2")
+                .header("Access-Token", "1111222validtoken33344444")
+        .when()
+                .get("/pet/{foundByStatus}", petStatus)
+        .then()
+                .statusCode(code)   //когда юзаю со статус кодом "200" GET запрос, то тест фейлиться - не пойму почему...
+                .body("code", is(404))
+                .body("type", is("unknown"))
+                .body("message", equalTo(message)); //в тесте не подставляется мессадж - ??
+
+     }
+
+     
+
+     @DataProvider(name = "Invalid statuses")
+     private Object[] [] provider() {
+        return new Object[][] {
+                        {"Books", 404, "Pets category not found"},
+                        {"Specialbooks!@#$%^&*", 404, "Invalid pets category"},
+        };
+     }
+
 }
